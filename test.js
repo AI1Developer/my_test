24a6c2dac73947add748978a16d87a0444487790
AI1Developer <160612516+AI1Developer@users.noreply.github.com>
2024-03-27 02:15:51 +0500

    update some line in  code
    thanks

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 const test = `I am Testing GitHub`;
 console.log(test);
+let you = 1234566543;
+console.log(you);
 export {};
--- test.ts
+++ test.ts
@@ -1,2 +1,4 @@
-const test=`I am Testing GitHub`
+const test:string=`I am Testing GitHub`
 console.log(test);
+let you:number=1234566543;
+console.log(you);
